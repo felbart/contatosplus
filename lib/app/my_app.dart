3dbf95cc748ae31037825585c5711acbb8e7353f
Felipe Leite <felbart@gmail.com>
2024-11-28 21:11:16 -0300

    atualizando detalhes home

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Contato Plus App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
