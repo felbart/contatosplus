9e3652d223a5b8b5b02673582538108483610013
Felipe Leite <felbart@gmail.com>
2024-12-03 07:44:47 -0300

    validando form

--- lib/app/view/contact_form.dart
+++ lib/app/view/contact_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class ContactForm extends StatefulWidget {
   const ContactForm({super.key});
@@ -8,6 +9,7 @@ class ContactForm extends StatefulWidget {
 }
 
 class _ContactFormState extends State<ContactForm> {
+  final _formKey = GlobalKey<FormState>();
   final _nomeController = TextEditingController();
   final _telefoneController = TextEditingController();
   final _emailController = TextEditingController();
@@ -21,7 +23,9 @@ class _ContactFormState extends State<ContactForm> {
           IconButton(
             icon: const Icon(Icons.save),
             onPressed: () {
-              _salvarContato();
+              if (_formKey.currentState!.validate()) {
+                _salvarContato();
+              }
             },
           ),
         ],
@@ -29,6 +33,7 @@ class _ContactFormState extends State<ContactForm> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -36,13 +41,27 @@ class _ContactFormState extends State<ContactForm> {
                 controller: _nomeController,
                 decoration: const InputDecoration(labelText: 'Nome'),
                 textInputAction: TextInputAction.next,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Informe um nome para o contato.';
+                  }
+                  return null;
+                },
               ),
-              const SizedBox(height: 16),
               TextFormField(
                 controller: _telefoneController,
                 decoration: const InputDecoration(labelText: 'Telefone'),
                 keyboardType: TextInputType.phone,
                 textInputAction: TextInputAction.next,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Você deve informar um telefone.';
+                  }
+                  if (!RegExp(r'^\d+$').hasMatch(value)) {
+                    return 'Insira apenas números.';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 16),
               TextFormField(
@@ -50,6 +69,15 @@ class _ContactFormState extends State<ContactForm> {
                 decoration: const InputDecoration(labelText: 'E-mail'),
                 keyboardType: TextInputType.emailAddress,
                 textInputAction: TextInputAction.done,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'O e-mail é obrigatório.';
+                  }
+                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Insira um email válido.';
+                  }
+                  return null;
+                },
               ),
             ],
           ),
